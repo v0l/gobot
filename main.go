2eea06cf367d2ebad843281edec8d939af9542b6
Kieran <kieran@harkin.me>
2016-05-13 17:42:12 +0100

    random?

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"net"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/domainr/whois"
 	"github.com/thoj/go-ircevent"
@@ -140,7 +141,7 @@ func OnPrivMsg(e *irc.Event) {
 			{
 				if len(cmd) > 1 {
 					ans := []string{"It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"}
-					rand.Seed(42)
+					rand.Seed(time.Now().UnixNano())
 					e.Connection.Privmsgf(args[0], "%v: %v", e.Nick, ans[rand.Intn(len(ans))])
 				}
 				break
