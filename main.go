3face5dc99bfe45b1d52ed6629221cce83e9fb80
Kieran <kieran@harkin.me>
2016-05-13 17:32:03 +0100

    add 8ball

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"net"
 	"strconv"
 	"strings"
@@ -132,6 +133,15 @@ func OnPrivMsg(e *irc.Event) {
 							e.Connection.Privmsgf(args[0], "[%v] %v %v", e.Nick, ip.String(), cmd[1])
 						}
 					}
+				}
+				break
+			}
+		case "!8":
+			{
+				if len(cmd) > 1 {
+					ans := []string{"It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"}
+					rand.Seed(42)
+					e.Connection.Privmsgf(args[0], "%v: %v", e.Nick, ans[rand.Intn(len(ans))])
 				}
 				break
 			}
